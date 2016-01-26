8f198da173e6db1461efe33396eb6d848e6940f0
Basti-Shi <1227401081@suda.edu.cn>
2016-01-26 21:01:42 +0800

    完成基本功能展示

--- app/src/main/java/com/basti/skinselector/SecondActivity.java
+++ app/src/main/java/com/basti/skinselector/SecondActivity.java
@@ -1,30 +1,31 @@
 package com.basti.skinselector;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.zhy.changeskin.SkinManager;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Bowen on 2016-01-26.
+ */
+public class SecondActivity extends AppCompatActivity {
 
     private Button bt_day, bt_night, bt_normal;
-    private TextView tv_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
         SkinManager.getInstance().register(this);
 
         initView();
 
         initEvent();
-
     }
 
     private void initEvent() {
@@ -48,14 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 SkinManager.getInstance().removeAnySkin();
             }
         });
-
-        tv_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SecondActivity.class));
-            }
-        });
-
     }
 
     private void initView() {
@@ -64,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
         bt_night = (Button) findViewById(R.id.night);
         bt_normal = (Button) findViewById(R.id.normal);
 
-        tv_next = (TextView) findViewById(R.id.next);
     }
 
     @Override
